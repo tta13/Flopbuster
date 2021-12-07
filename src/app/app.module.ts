d26a99b3b6ab580a7d7cea85623306f6b20a7783
Tales Alves <tta@cin.ufpe.br>
2021-12-07 11:18:20 -0300

    Creating flop-votes component and moving logic from app-root to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FlopVotesComponent } from './flop-votes/flop-votes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlopVotesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/flop-votes/flop-votes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FlopVoteService } from './flop-vote.service';
+
+@Component({
+  selector: 'app-flop-votes',
+  templateUrl: './flop-votes.component.html',
+  styleUrls: ['./flop-votes.component.css']
+})
+export class FlopVotesComponent implements OnInit {
+
+  title = 'Flop Votes!';
+
+  constructor(private flopVoteService: FlopVoteService) { }
+
+  ngOnInit(): void {
+    this.flopVoteService = new FlopVoteService();
+  }
+
+  retrieveMovieList() { return this.flopVoteService.movieList; }
+
+  choseMovie(): boolean { return this.flopVoteService.choseMovie; }
+
+  chooseMovie(event, movie){
+    this.flopVoteService.chooseMovie(event, movie);
+  }
+
+}
